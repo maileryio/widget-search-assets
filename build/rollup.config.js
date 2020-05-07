3c718cb5593b20050e1b936f8899ba3c03c20ba9
Igor Maliy <imaliy.filsh@gmail.com>
2020-05-07 23:03:13 +0300

    Initial commit

---
+++ build/rollup.config.js
@@ -0,0 +1,53 @@
+import commonjs from 'rollup-plugin-commonjs';
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import { eslint } from 'rollup-plugin-eslint';
+import sassPostcss from 'rollup-plugin-sass-postcss';
+import autoprefixer from 'autoprefixer';
+
+const env = process.env.NODE_ENV || 'development';
+const isProd = env === 'production';
+
+export default (async () => ({
+  input: 'src/index.js',
+  output: {
+    name: '_____',
+    exports: 'named',
+    sourcemap: true,
+    globals: {
+      'bootstrap-vue': 'BootstrapVue',
+      'vue': 'Vue',
+      'vuex': 'Vuex'
+    }
+  },
+  external: [
+    'bootstrap-vue',
+    'vue',
+    'vuex'
+  ],
+  plugins: [
+    eslint(),
+    commonjs(),
+    resolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    sassPostcss({
+      output: 'dist/main.min.css',
+      sourcemap: true,
+      plugins: [
+        autoprefixer()
+      ]
+    }),
+    vue({
+      css: true,
+      compileTemplate: true
+    }),
+    buble({
+      objectAssign: 'Object.assign'
+    }),
+    isProd && (await import('rollup-plugin-terser')).terser()
+  ]
+}))();
